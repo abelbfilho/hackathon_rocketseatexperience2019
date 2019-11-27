cdd4bbdc71357fff086d223f34320e1a803daa72
Abel Babini Filho <abelbfilho@hotmail.com>
2019-11-27 09:57:05 -0300

    adding try catch

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -6,46 +6,32 @@ import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
-    const { email, password } = req.body;
+    try {
+      const { email, password } = req.body;
 
-    const user = await User.findOne({
-      where: { email },
-      include: [
-        {
-          model: File,
-          as: 'avatar',
-          attributes: ['id', 'path', 'url'],
-        },
-      ],
-    });
+      const user = await User.findOne({
+        where: { email },
+        include: [
+          {
+            model: File,
+            as: 'avatar',
+            attributes: ['id', 'path', 'url'],
+          },
+        ],
+      });
 
-    if (!user) {
-      return res.status(401).json({ error: 'User not found' });
-    }
+      if (!user) {
+        return res.status(401).json({ error: 'User not found' });
+      }
 
-    if (!(await user.checkPassword(password))) {
-      return res.status(401).json({ error: 'Password does not match' });
-    }
+      if (!(await user.checkPassword(password))) {
+        return res.status(401).json({ error: 'Password does not match' });
+      }
 
-    const {
-      id,
-      name,
-      lastname,
-      state,
-      city,
-      phone,
-      points,
-      classification,
-      user_company,
-      avatar_id,
-    } = user;
-
-    return res.json({
-      user: {
+      const {
         id,
         name,
         lastname,
-        email,
         state,
         city,
         phone,
@@ -53,11 +39,29 @@ class SessionController {
         classification,
         user_company,
         avatar_id,
-      },
-      token: jwt.sign({ id }, authConfig.secret, {
-        expiresIn: authConfig.expiresIn,
-      }),
-    });
+      } = user;
+
+      return res.json({
+        user: {
+          id,
+          name,
+          lastname,
+          email,
+          state,
+          city,
+          phone,
+          points,
+          classification,
+          user_company,
+          avatar_id,
+        },
+        token: jwt.sign({ id }, authConfig.secret, {
+          expiresIn: authConfig.expiresIn,
+        }),
+      });
+    } catch (err) {
+      return res.json(err);
+    }
   }
 }
 
