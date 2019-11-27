5227b8649c18f383cc59b5e4d14493a59cf200f0
Abel Babini Filho <abelbfilho@hotmail.com>
2019-11-27 09:42:09 -0300

    new submit to heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,28 +1,15 @@
 require('dotenv/config');
 
 module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    operatorsAliases: false,
+  use_env_variable: 'DATABASE_URL',
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  define: {
+    timestamps: true,
     underscored: true,
     underscoredAll: true,
-  },
-  test: {
-    username: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    dialect: 'postgres',
-    operatorsAliases: false,
-  },
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    operatorsAliases: false,
   },
 };
