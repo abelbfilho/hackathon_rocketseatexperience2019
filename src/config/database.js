6eeab3b2ef38c5992f332416aed63051eb18a9ff
Abel Babini Filho <abelbfilho@hotmail.com>
2019-11-27 10:08:15 -0300

    new heroku configs

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 require('dotenv/config');
 
 module.exports = {
-  use_env_variable: 'DATABASE_URL',
+  use_env_variable: process.env.DATABASE_URL,
   dialect: process.env.DB_DIALECT,
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
