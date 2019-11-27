61c191431ed7a4d688fabd531f72d7ed8b7385b7
Abel Babini Filho <abelbfilho@hotmail.com>
2019-11-27 10:28:06 -0300

    test

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,6 @@
 require('dotenv/config');
 
 module.exports = {
-  use_env_variable: process.env.DATABASE_URL,
   dialect: process.env.DB_DIALECT,
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
