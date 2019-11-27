a41bedb84a6d8d5c3dcf2c4805663bc2b669d74e
Abel Babini Filho <abelbfilho@hotmail.com>
2019-11-27 09:28:37 -0300

    adjusting database connection config

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,28 @@
 require('dotenv/config');
 
 module.exports = {
-  dialect: process.env.DB_DIALECT,
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  define: {
-    timestamps: true,
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    operatorsAliases: false,
     underscored: true,
     underscoredAll: true,
+  },
+  test: {
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    dialect: 'postgres',
+    operatorsAliases: false,
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    operatorsAliases: false,
   },
 };
